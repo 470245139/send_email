de51a66de31010c671516d879371c48495cd51d4
470245139 <470245139@qq.com>
2023-11-27 17:22:35 +0800

    第一次提交。

--- main1.py
+++ main1.py
@@ -5,3 +5,5 @@ yag = yagmail.SMTP(user="ffffz470245139@163.com", password="CZNBRJEPGWLDUQZF", h
 contents = ['今天是周末,我要学习, 学习使我快乐;', '<a href="https://www.python.org/">python官网的超链接</a>', './girl.png']
 
 yag.send('470245139@qq.com', '主题:学习使我快乐', contents)
+
+
